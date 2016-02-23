782501f2ca36ed0f1207864a95bf3026afa07e5c
Nicky Cadavillo <nicoleacadavillo@gmail.com>
2016-02-23 17:05:23 -0500

    lodash works

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -26,15 +26,57 @@ $('.tabList').each(function(){ //iterate though
 /********************** lodash ****************************/
 
 $.getJSON('https://api.github.com/users/octocat', function(json){
+//image
+  var image = _.template('<%- m.avatar_url %>', {variable: 'm'});
+  var show = image({avatar_url: json.avatar_url});
+  $(".photo").html(image);
 
+//login
     var name = _.template('<%- m.login %>', {variable: 'm'});
     var render = name({login: json.login});
     $(".name").html(render);
 
-});
+//name
+    var fullName = _.template('<%- m.name %>', {variable: 'm'});
+    var renderFullName = fullName({name: json.name});
+    $(".full_name").html(renderFullName);
+
+//company
+    var company = _.template('<%- m.company %>', {variable: 'm'});
+    var renderCompany = fullName({company: json.company});
+    $(".company").html(renderCompany);
+
+// email
+    var email = _.template('<%- m.email %>', {variable: 'm'});
+    var renderEmail = email({email: json.email});
+    $(".email").html(renderEmail);
+
+// location
+    var location = _.template('<%- m.location %>', {variable: 'm'});
+    var renderLocation = location({location: json.location});
+    $(".location").html(renderLocation);
+
+// blog
+    var blog = _.template('<%- m.blog %>', {variable: 'm'});
+    var renderBlog = blog({blog: json.blog});
+    $(".blog").html(renderBlog);
 
 
+// followers
+  var followers = _.template('<%- m.followers %>', {variable: 'm'});
+  var renderFollowers = followers({followers: json.followers});
+  $("#followers").html(renderFollowers);
 
+// starred
+  // var starred = _.template('<%- m.starred_url %>', {variable: 'm'});
+  // var renderStarred = starred({starred_url: json.starred_url});
+  // $("#starred").html(renderStarred);
 
+// followers
+  var following = _.template('<%- m.following %>', {variable: 'm'});
+  var renderFollowing = following({following: json.following});
+  $("#following").html(renderFollowing);
+
+  });
 
 });
